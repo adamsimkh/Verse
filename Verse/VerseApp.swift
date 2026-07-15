0ae800750cc082384f511aa7e1a2a5088b2f1726
adamsimkh <preciousadamsimuekemhe@gmail.com>
2026-07-15 12:30:24 +0100

    Initial Commit

---
+++ Verse/VerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerseApp.swift
+//  Verse
+//
+//  Created by Adams on 15/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct VerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
